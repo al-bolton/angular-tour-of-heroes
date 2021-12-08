d443444be931028825b49d8d60373706c134bffc
Alex Bolton <boltonalex89@gmail.com>
2021-12-08 16:13:02 +0000

    Finished tutorial up to step 4

---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from './message.service';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  constructor(private messageService: MessageService) { }
+}
